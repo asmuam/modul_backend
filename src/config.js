e57457cc94f72274b431bda96adc5900bc4b0b0d
asmuammal <asmuammal@gmail.com>
2024-09-27 03:57:25 +0700

    chore(all): tambah dotenv

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.ACCESS_TOKEN_SECRET,
   refreshSecret: process.env.REFRESH_TOKEN_SECRET,
   domain: process.env.DOMAIN,
+  node: process.env.NODE_ENV,
 };
 
 export default config;
